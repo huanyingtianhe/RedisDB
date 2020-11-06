c1d319d75e3da7ef2d08da7fa2feb2435c86318d
Lei Wu <wule@microsoft.com>
2020-11-06 17:35:09 +0800

    modify skiplist namespace

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,6 @@
 
 int main(int, char**) {
     std::cout << "Hello, world!\n";
-    Redis::SkipList<int, int> skiplist;
+    RedisDataStructure::SkipList<int, int> skiplist;
     std::cout << "skiplist.maxLevel"<<skiplist.getMaxLevel()<<std::endl;
 }
